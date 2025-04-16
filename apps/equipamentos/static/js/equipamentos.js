4226dc07ae35d4f8f9033918a9a1a346d9056aab
ianroddrs <ianroddrs@gmail.com>
2025-04-16 12:10:36 -0300

    removido tomselect

--- apps/equipamentos/static/js/equipamentos.js
+++ apps/equipamentos/static/js/equipamentos.js
@@ -12,42 +12,38 @@ if(modal){
 
         modalBody.innerHTML = formulario
 
-        modalBody.querySelectorAll('select').forEach(select => {
-            new TomSelect(select, {
-                placeholder: `Selecione uma opção`,
-                create: true,
-                createOnBlur: true,     
-                render: {
-                    option_create: function(data, escape) {
-                        return `<div class="create bg-white">Adicionar <strong>${escape(data.input)}</strong>&hellip;</div>`;
-                    },
-                    no_results: function() {
-                        return '<div class="no-results bg-white">Nenhum resultado encontrado</div>';
-                    },
-                    item: function(data) {
-                        return `<div class="tom-select-item rounded bg-white">${data.valor.toUpperCase()}</div>`;
-                    },
-                    option: function(data) {
-                        return `<div class="tom-select-option rounded bg-white">${data.text}</div>`;
-                    },
-                    dropdown: function() {
-                        return '<div class="rounded-3 border p-2 bg-white"></div>';
-                    }
-                },
-                valueField: "id",
-                labelField: "valor",
-                searchField: "valor",
-                load: function(query, callback){
-                    fetch(`/api/localidades?${select.name}=${encodeURIComponent(query)}`)
-                        .then(response => response.json())
-                        .then(json => callback(json))
-                        .catch(() => callback)
-                }
-            })
+        modalBody.querySelectorAll('select').forEach(async select => {
+            // let localidades = await fetch(`/api/localidades?${select.name}=${encodeURIComponent(query)}`)
+            try {
+                const response = await fetch('/api/localidades');
+                const json = await response.json();
+                localidades = json;
+                callback(json); // optionally call the callback
+              } catch (error) {
+                console.error('Fetch error:', error);
+                callback(null); // or handle error as needed
+              }
+              
+              console.log(localidades);
         })
     })
 }
 
 async function enviarDados() {
-    document.querySelector('form').submit()
+    let formulario = document.querySelector('form')
+    let inputs = formulario.querySelectorAll('input, select, textarea')
+    let submit= true
+
+    inputs.forEach(input => {
+        if(input.required && !input.value){
+            input.classList.add('is-invalid')
+            submit = false
+        }
+    })
+
+    if(submit){
+        formulario.submit()
+    }else{
+        alert('Preencha os campos obrigatórios')
+    }
 }
